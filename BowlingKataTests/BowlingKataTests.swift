0953f157941dd2d2daf826ab92637e37106c5d51
Choi Hyun <Francis@Jinhos-MacBook-Air.local>
2017-01-18 17:18:19 +0900

    the first commit - bowling kata for mac

--- BowlingKataTests/BowlingKataTests.swift
+++ BowlingKataTests/BowlingKataTests.swift
@@ -22,15 +22,15 @@ class BowlingKataTests: XCTestCase {
     }
     
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let viewController = ViewController()
+        XCTAssertTrue(viewController.isNumberEven(num: 6))
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
     
 }
