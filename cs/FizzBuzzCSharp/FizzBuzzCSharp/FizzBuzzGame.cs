645883157b7727f3fa41c1b096b28fd77c4d78c4
cjhnim <cjhnim@gmail.com>
2017-02-03 13:44:41 +0900

    the some projects was commited and was moved.

---
+++ cs/FizzBuzzCSharp/FizzBuzzCSharp/FizzBuzzGame.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace FizzBuzzCSharp
+{
+    internal class FizzBuzzGame
+    {
+        internal static String Play(int number)
+        {
+            if (IsFizzBuzz(number))
+            {
+                return "FizzBuzz";
+            }
+            else if (IsBuzz(number))
+            {
+                return "Buzz";
+            }
+            else if (IsFizz(number))
+            {
+                return "Fizz";
+            }
+            else
+            {
+                return number.ToString();
+            }
+        }
+
+        #region Private Methods
+        private static bool IsBuzz(int number)
+        {
+            return (number % 5) == 0;
+        }
+
+        private static bool IsFizz(int number)
+        {
+            return (number % 3) == 0;
+        }
+
+        private static bool IsFizzBuzz(int number)
+        {
+            return (number % 15) == 0;
+        }
+        #endregion
+    }
+}
---
+++ cs/FizzBuzzCSharp/FizzBuzzCSharp/TestFizzBuzzGame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace FizzBuzzCSharp
+{
+    [TestFixture]
+    [Category("TheFizzBuzzKata")]
+    public class TestFizzBuzzGameRA
+    {
+        [Test]
+        [TestCase(1, "1")]
+        [TestCase(3, "Fizz")]
+        [TestCase(5, "Buzz")]
+        [TestCase(15, "FizzBuzz")]
+        [TestCase(30, "FizzBuzz")]
+        public void 경계값위주숫자를입력하여결과값을확인한다(int number, string expectedResult)
+        {
+            Assert.AreEqual(expectedResult, FizzBuzzGame.Play(number));
+        }
+    }
+}
